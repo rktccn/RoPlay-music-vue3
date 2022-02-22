1e54d98cba8eef956600fd1126a90bea1b1e3439
wskkku <78215185+wskkku@users.noreply.github.com>
2022-02-22 23:13:00 +0800

    增加功能

--- src/views/feed/components/recomendOther.vue
+++ src/views/feed/components/recomendOther.vue
@@ -1,47 +1,93 @@
 <template lang="">
-  <section>
-    <h4 class="text-style-title">音乐</h4>
+<section>
+    <h4 class="text-style-title">视&nbsp;·&nbsp;听</h4>
     <div class="container">
       <div class="left-video">
-        <div class="video-container">
-          <CarouselList :length="video.length" v-if="video" :rows="2">
+        <div class="video-container" ref="videoItem">
+          <CarouselList :length="video.length" v-if="video" :rows="2" key="video">
             <template v-for="(item, index) in video" :key="index">
-            <div class="video-list">
-              <PlayListCard :item="item" type="video"></PlayListCard>
-            </div>
+              <div class="video-list">
+                <PlayListCard :item="item" type="video"></PlayListCard>
+              </div>
             </template>
           </CarouselList>
         </div>
       </div>
-      <div class="right-song"></div>
+      <div class="right-song">
+        <CarouselList :length="song.length" v-if="song && songRows !== 0" :rows="songRows" key="song" >
+          <template v-for="(value, index) in song" :key="index">
+            <TrackListItem class="track-item" :height="songItemHeight" :item="value"></TrackListItem>
+          </template>
+        </CarouselList>
+      </div>
     </div>
   </section>
 </template>
+
 <script>
-import { reactive, toRefs } from "vue";
+import { onMounted, reactive, ref, toRefs } from "vue";
 import { ToplistOfMV } from "../../../apis/mv";
+import { recommendNewSong } from "../../../apis/personalized";
+
 import CarouselList from "../../../components/carouselList.vue";
+import TrackListItem from "../../../components/trackListItem.vue";
 
 export default {
   setup() {
     const data = reactive({
       video: null,
-      numm:[1,2,3,4,5,6,7,8,9,10]
+      song: null,
+      numm: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      songRows: 0,
+      songItemHeight: "64px",
     });
 
-    ToplistOfMV().then((res) => {
-      data.video = res.data;
+    ToplistOfMV()
+      .then((res) => {
+        data.video = res.data;
+      })
+      .then(() => {
+        setSongRows();
+      });
+
+    recommendNewSong().then((res) => {
+      data.song = res.result;
     });
 
-    return { ...toRefs(data) };
+    const videoItem = ref(null);
+
+    // 设置歌曲列表显示列数
+    const setSongRows = () => {
+      if (window.innerWidth < 1250) {
+        data.songRows = 2;
+        return;
+      }
+      console.log(data.songRows);
+
+      let height = videoItem.value.offsetHeight;
+      let size = height / 70;
+      size = Math.floor(size);
+      data.songRows = size;
+      data.songItemHeight = height / size + "px";
+    };
+
+    onMounted(() => {
+      window.addEventListener("resize", () => setSongRows(), false);
+    });
+
+    return { ...toRefs(data), videoItem };
   },
-  components: { CarouselList },
+  components: { CarouselList, TrackListItem },
 };
 </script>
 <style lang="scss" scoped>
 .container {
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: $lg) {
+    flex-direction: column;
+  }
 }
 .left-video,
 .right-song {
@@ -49,9 +95,11 @@ export default {
   border-radius: $border-radius-default;
   padding: 8px 12px;
 }
-
 .left-video {
   @include calc-width(6.9);
+  @media (max-width: $lg) {
+    @include calc-width(10);
+  }
 
   .video-container {
     .video-list {
@@ -62,5 +110,15 @@ export default {
 }
 .right-song {
   @include calc-width(2.9);
+  @media (max-width: $lg) {
+    margin-top: 12px;
+    @include calc-width(10);
+  }
+  .track-item {
+    @include calc-width(2.5);
+    @media (max-width: $lg) {
+      @include calc-width(4.8);
+    }
+  }
 }
 </style>
